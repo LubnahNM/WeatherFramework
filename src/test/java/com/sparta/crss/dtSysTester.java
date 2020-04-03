9794c9f6c8a54e3cc6a8f6713f091be7dd145080
Sebastian Fenesan <sebi96bv@gmail.com>
2020-04-03 13:44:41 +0100

    Test for unix date and sys

---
+++ src/test/java/com/sparta/crss/dtSysTester.java
@@ -0,0 +1,54 @@
+package com.sparta.crss;
+
+import com.sparta.crss.DataInjection.RequestHandler;
+import com.sparta.crss.JacksonClasses.WeatherDTOJackson;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class dtSysTester {
+
+    private WeatherDTOJackson weatherQuery;
+    private RequestHandler requestHandler;
+
+    @BeforeEach
+    public void setUp() {
+        requestHandler = new RequestHandler("weather?q=London,uk");
+        weatherQuery = requestHandler.createResult();
+    }
+
+    @Test
+    public void testDtType() {
+        Assertions.assertEquals(Integer.class, weatherQuery.getDt().getClass());
+    }
+
+    @Test
+    public void testSysTypeType() {
+        Assertions.assertEquals(Integer.class, weatherQuery.getSys().getType().getClass());
+    }
+
+    @Test
+    public void testSysIdType() {
+        Assertions.assertEquals(Integer.class, weatherQuery.getSys().getId().getClass());
+    }
+
+/*    @Test
+    public void testSysMessageType() {
+        Assertions.assertEquals(null, weatherQuery.getSys().getMessage().getClass());
+    }*/
+
+    @Test
+    public void testSysCountryType() {
+        Assertions.assertEquals(String.class, weatherQuery.getSys().getCountry().getClass());
+    }
+
+    @Test
+    public void testSysSunriseType() {
+        Assertions.assertEquals(Integer.class, weatherQuery.getSys().getSunrise().getClass());
+    }
+
+    @Test
+    public void testSysSunsetType() {
+        Assertions.assertEquals(Integer.class, weatherQuery.getSys().getSunset().getClass());
+    }
+}
