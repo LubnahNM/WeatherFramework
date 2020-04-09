ddbeda6814298d6236303eae5da0277d2fd543b7
Tolga Komur <tkomur@spartaglobal.com>
2020-04-09 15:17:17 +0100

    Made improvements to Tester classes

--- src/test/java/com/sparta/crss/JacksonClasses/Rain.java
+++ src/test/java/com/sparta/crss/JacksonClasses/Rain.java
@@ -11,17 +11,24 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-        "1h",
-        "3h"
+        "1h"
 })
 public class Rain {
 
     @JsonProperty("1h")
-    public Double _1h;
-    @JsonProperty("3h")
-    public Double _3h;
+    private Double _1h;
     @JsonIgnore
     private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("1h")
+    public Double get1h() {
+        return _1h;
+    }
+
+    @JsonProperty("1h")
+    public void set1h(Double _1h) {
+        this._1h = _1h;
+    }
 
     @JsonAnyGetter
     public Map<String, Object> getAdditionalProperties() {
